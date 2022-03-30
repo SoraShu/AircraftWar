8f136d9f87823bee551dac65c4c6230f724d4c63
SoraShu <sorashu@outlook.com>
2022-03-30 16:04:51 +0800

    refactor: Singleton Pattern for HeroCraft

--- src/edu/hitsz/application/Game.java
+++ src/edu/hitsz/application/Game.java
@@ -54,10 +54,7 @@ public class Game extends JPanel {
 
 
     public Game() {
-        heroAircraft = new HeroAircraft(
-                Main.WINDOW_WIDTH / 2,
-                Main.WINDOW_HEIGHT - ImageManager.HERO_IMAGE.getHeight(),
-                0, 0, 100);
+        heroAircraft = HeroAircraft.getHeroAircraft();
 
         enemyAircrafts = new LinkedList<>();
         heroBullets = new LinkedList<>();
